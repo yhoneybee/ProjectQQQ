31ee809c8e27e97d18896953b55389b82781a85b
chae young hoon <chaeyh04@gmail.com>
2021-12-27 16:02:48 +0900

    FPS

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private bool isMy;
+    [SerializeField] private float speed;
+    [SerializeField] private CinemachineVirtualCamera vcam;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isMy) return;
+
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+
+        transform.position += (transform.right * h + transform.forward * v) * speed * Time.deltaTime;
+        transform.Rotate(0, x * 2, 0);
+        var euler = Quaternion.Euler(vcam.transform.rotation.eulerAngles.x - y * 2, transform.rotation.eulerAngles.y, 0);
+        vcam.transform.rotation = euler;
     }
 }
