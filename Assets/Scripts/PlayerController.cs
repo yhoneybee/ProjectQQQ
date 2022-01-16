e3c534e6bf39a87a399f093373334983ad6b8551
chae young hoon <chaeyh04@gmail.com>
2022-01-16 18:05:38 +0900

    Update Ingame

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float crouchSpeed;
     [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float moveSpeed;
+    private bool isMove;
+    private bool IsRun
+    {
+        get => isRun;
+        set
+        {
+            if (IsCrouch) return;
+            isRun = value;
+            moveSpeed = isRun ? runSpeed : walkSpeed;
+        }
+    }
+    private bool isRun;
+    private bool IsGround => Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    private bool IsCrouch
+    {
+        get => isCrouch;
+        set
+        {
+            isCrouch = value;
+            moveSpeed = isCrouch ? crouchSpeed : walkSpeed;
+            StopAllCoroutines();
+            StartCoroutine(ECrouch(isCrouch ? crouchPosY : originPosY));
+        }
+    }
+    private bool isCrouch;
+    [SerializeField] private float crouchPosY;
+    [SerializeField] private float originPosY;
+    [SerializeField] private float jumpForce;
     [SerializeField] private float lookSensitivity;
     [SerializeField] private float cameraRotationLimit;
     private float CurrentCameraRotationX
@@ -18,6 +49,7 @@ public class PlayerController : MonoBehaviour
     }
     private float currentCameraRotationX;
 
+    [SerializeField] private CapsuleCollider capsuleCollider;
     [SerializeField] private Camera theCamera;
     [SerializeField] private Rigidbody rigid;
 
@@ -26,15 +58,52 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         if (!rigid) rigid = GetComponent<Rigidbody>();
+        IsRun = false;
+        IsCrouch = false;
     }
 
     private void Update()
     {
-        Move();
+        if (transform.CompareTag("OtherPlayer"))
+        {
+            // TODO : Server에서 위치와 회전값을 받고 적용시키기
+        }
+        else
+        {
+            if (!IsGround) IsCrouch = false;
+            else if (Input.GetKey(KeyCode.LeftControl)) IsCrouch = true;
+            else IsCrouch = false;
 
-        CameraRotation();
+            if (Input.GetKey(KeyCode.Space) && IsGround)
+                rigid.velocity = transform.up * jumpForce;
 
-        CharacterRotation();
+            TryRun();
+
+            Move();
+
+            CameraRotation();
+
+            CharacterRotation();
+        }
+    }
+
+    private IEnumerator ECrouch(float posY)
+    {
+        var wait = new WaitForSeconds(0.01f);
+        while (Mathf.Abs(theCamera.transform.localPosition.y - posY) > 0.01f)
+        {
+            theCamera.transform.localPosition = Vector3.Lerp(theCamera.transform.localPosition, Vector3.up * posY, Time.deltaTime * 15);
+            yield return wait;
+        }
+        theCamera.transform.localPosition = Vector3.up * posY;
+    }
+
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+            IsRun = true;
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+            IsRun = false;
     }
 
     private void CharacterRotation()
@@ -46,8 +115,7 @@ public class PlayerController : MonoBehaviour
     private void CameraRotation()
     {
         float rotationX = Input.GetAxisRaw("Mouse Y");
-        CurrentCameraRotationX -= rotationX * lookSensitivity;
-        CurrentCameraRotationX = Mathf.Clamp(CurrentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+        CurrentCameraRotationX = Mathf.Clamp(CurrentCameraRotationX - rotationX * lookSensitivity, -cameraRotationLimit, cameraRotationLimit);
     }
 
     private void Move()
@@ -58,6 +126,8 @@ public class PlayerController : MonoBehaviour
         var x = transform.right * h;
         var z = transform.forward * v;
 
-        rigid.MovePosition(transform.position + (x + z).normalized * walkSpeed * Time.deltaTime);
+        rigid.MovePosition(transform.position + (x + z).normalized * moveSpeed * Time.deltaTime);
+
+        isMove = h != 0 || v != 0;
     }
 }
